7a54f9db954b5ae4af57c927bac8389fcf3201dd
Gaoli Chen <galic.chen@gmail.com>
2015-07-15 01:02:13 -0400

    delete common matlab functions from boson folder.

--- matlab/boson/lowest_energies2.m
+++
@@ -1,5 +0,0 @@
-function f=lowest_energies2(bits, n, statenumber)
-    [Vec,D] = eigs(pham(bits, n), statenumber, 'sr');
-    state_norm = real(diag(ctranspose(Vec)*metric(bits, n)*Vec));
-    f = sortrows(horzcat(real(diag(D)),imag(diag(D)), state_norm), 1);
-end
