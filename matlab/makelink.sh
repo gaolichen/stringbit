6ddd1762dbb929a54af947c55af4853271433ea6
Gaoli Chen <galic.chen@gmail.com>
2015-07-15 16:22:30 -0400

    reorg the matlab script

---
+++ matlab/makelink.sh
@@ -0,0 +1,13 @@
+ln -s $1/find_match.m find_match.m
+ln -s $1/get_state.m get_state.m
+ln -s $1/get_state2.m get_state2.m
+ln -s $1/lowest_energies2.m lowest_energies2.m
+ln -s $1/lowest_energies.m lowest_energies.m
+ln -s $1/missing_states2.m missing_states2.m
+ln -s $1/missing_states.m missing_states.m
+ln -s $1/plot_states2.m plot_states2.m
+ln -s $1/plot_states.m plot_states.m
+ln -s $1/metric.m metric.m
+ln -s $1/ham.m ham.m
+ln -s $1/pham.m pham.m
+
