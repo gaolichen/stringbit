c963c65e394295d7c7bde42fa82eb0aefb474c87
Gaoli Chen <galic.chen@gmail.com>
2015-07-16 17:40:43 -0400

    add groud_vanish_N.m and norm10.m norm11.m for boson

---
+++ matlab/ground_vanish_N2.m
@@ -0,0 +1,15 @@
+function f = ground_vanish_N2(maxBit)
+    ret = zeros(maxBit - 2, 2);
+    for bit = 3 : maxBit
+        ret(bit - 2, 1) = bit;
+        for n = bit : -0.5 : 0.5
+            state = lowest_energies2(bit, n, 1);
+            if state(1, 3) < 1e-8 || abs(state(1, 2)) > 1e-8
+                ret(bit - 2, 2) = n;
+                break;
+            end
+        end
+    end
+    
+    f = ret;
+end
