741692bc17277f7d96b06e96847199850f404853
Gaoli Chen <galic.chen@gmail.com>
2015-07-18 17:46:48 -0400

    add fermion norm10.m and some matlab functions.

--- matlab/ground_vanish_N2.m
+++ matlab/ground_vanish_N2.m
@@ -1,8 +1,12 @@
-function f = ground_vanish_N2(maxBit)
+function f = ground_vanish_N2(maxBit, maxN)
+    if nargin < 2
+        maxN = maxBit;
+    end
+    
     ret = zeros(maxBit - 2, 2);
     for bit = 3 : maxBit
         ret(bit - 2, 1) = bit;
-        for n = bit : -0.5 : 0.5
+        for n = min(bit, maxN) : -0.5 : 0.5
             state = lowest_energies2(bit, n, 1);
             if state(1, 3) < 1e-8 || abs(state(1, 2)) > 1e-8
                 ret(bit - 2, 2) = n;
